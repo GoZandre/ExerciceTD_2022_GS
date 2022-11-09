cd22956cbdbd8ffb26aade3a045576690c4d72c2
Cha <wsh@oo.com>
2022-11-09 12:33:29 +0100

    ADD Basics tower defense feaatures cool af

---
+++ TowerDefenseWorkshop_Template/Assets/TD/Scripts/Rythm/Damager.cs
@@ -0,0 +1,12 @@
+namespace GSGD1
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Damager : AProjectile
+    {
+       
+    }
+
+}
